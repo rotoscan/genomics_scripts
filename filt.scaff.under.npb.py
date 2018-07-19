c832659cbd6d021658903777eb7059782f7ca7e9
Rodolfo Toscan <32571450+rotoscan@users.noreply.github.com>
2018-07-19 10:56:49 +0200

    Update filt.scaff.under.npb.py

--- filt.scaff.under.npb.py
+++ filt.scaff.under.npb.py
@@ -6,26 +6,24 @@
 # $ python chop.scaff.under.npb.py <assembly.fa> <assembly_filtered.fa> 5000
 #
 
-import os
-import sys
-from itertools import groupby
+import sys                      #
+from itertools import groupby   # import some  modules
 def filt_5kb(input_fasta,output_fasta,size):
-  #fin = open(input_fasta,"r")
-  #fout = open(input_fasta.replace(input_fasta.split("/")[-1],"filt_5kb_"+input_fasta.split("/")[-1]),"w")
-  fout = open(output_fasta,"w")
-  fin = open(input_fasta)
+  fout = open(output_fasta,"w")    # open output file to write
+  fin = open(input_fasta)          # open input file to read
   faiter = (x[1] for x in groupby(fin, lambda line: line[0] == ">"))
   for header in faiter:
-    # drop the ">"
     header = header.next()[1:].strip()
     # join all sequence lines to one.
     seq = "".join(s.strip() for s in faiter.next())
-    #header = str(">"+header)
     seq = str(seq)
     seqlen = len(seq)
     if seqlen >= int(size):
       fout.write(">"+header+"\n")
       fout.write(seq+"\n")
+    fin.close()                   # 
+    fout.close()                  # close input and output files
+    
 filt_5kb(sys.argv[1],sys.argv[2],sys.argv[3])
 
 
